f9cf2c59fe3893b4a88e31287c753650e074befd
yusei-wy <yusei.kasa@gmail.com>
2020-10-04 18:16:55 +0900

    change: TDD-4

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -1,11 +1,18 @@
 import { formatDate } from './date';
 
 describe('formatDate', () => {
-    const now = new Date('2020-08-25 17:58:53');
+    const now = new Date('2020/08/25 17:58:53');
     test('作成してすぐ', () => {
         expect(formatDate(new Date(now), now)).toEqual({
             datetime: '2020/08/25 17:58',
             isNew: true,
+        });
+    });
+
+    test('1週間経過', () => {
+        expect(formatDate(new Date('2020/08/18 17:58:53'), now)).toEqual({
+            datetime: '2020/08/18 17:58',
+            isNew: false,
         });
     });
 });
