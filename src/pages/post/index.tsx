65d681058d085bdf3cc57c8eae2e7e27344d6077
yusei-wy <yusei.kasa@gmail.com>
2020-10-08 12:47:49 +0900

    add: 投稿1

---
+++ src/pages/post/index.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from 'next';
+
+import styles from './index.module.css';
+
+const PostPage: NextPage = () => {
+    return (
+        <div className={styles.editContent}>
+            <input
+                className={styles.subject}
+                type="text"
+                placeholder="タ　イ　ト　ル"
+            />
+            <textarea
+                className={styles.editor}
+                placeholder="本文を書きましょう"
+            />
+        </div>
+    );
+};
+
+export default PostPage;
