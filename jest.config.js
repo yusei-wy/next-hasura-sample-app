2dc690759669e1432e5480b4409e81e1fb356958
yusei-wy <yusei.kasa@gmail.com>
2020-10-10 21:41:41 +0900

    fix: tsx のテストに失敗していた

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    transform: {
+        '.tsx?$': 'ts-jest',
+    },
     globals: {
         'ts-jest': {
             diagnostics: false,
         },
     },
+    // Setup Enzyme
+    snapshotSerializers: ['enzyme-to-json/serializer'],
+    setupFilesAfterEnv: ['<rootDir>/src/setupEnzyme.ts'],
 };
