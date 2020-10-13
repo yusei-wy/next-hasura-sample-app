624498909b895c702ff65d213f75317ca31bdbd4
yusei-wy <yusei.kasa@gmail.com>
2020-10-13 21:25:25 +0900

    add: 投稿7 投稿ボタンで投稿できるようにした

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,15 +1,20 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import React, { useCallback, useState } from 'react';
 
 import { Editor } from '@/components/editor';
 import { SiteHeader, SiteHeaderItem } from '@/components/site-header';
 import { Button } from '@/components/button';
+import { usePostArticleMutation } from '@/generated/graphql';
 
 import styles from './index.module.css';
 
 const PostPage: NextPage = () => {
     const [subject, setSubject] = useState('');
     const [content, setContent] = useState('');
+    const [postDisabled, setPostDisabled] = useState(false);
+    const [postArticle] = usePostArticleMutation();
+    const router = useRouter();
 
     const handleChangeSubject = useCallback(
         (ev: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,12 +23,41 @@ const PostPage: NextPage = () => {
         [],
     );
 
+    const handlePost = useCallback(
+        async (ev: React.FormEvent<HTMLFormElement>) => {
+            ev.preventDefault();
+            if (!content || !subject || postDisabled) return;
+            setPostDisabled(true);
+
+            const { data } = await postArticle({
+                variables: {
+                    // FIXME: authorID は一旦決め打ちにする
+                    authorID: '6916e23e-261f-46ad-840d-98e84db65c5d',
+                    content,
+                    subject,
+                    publishedAt: 'now()',
+                },
+            });
+            if (data && data.insert_articles_one) {
+                const articleID = data.insert_articles_one.id;
+                // FIXME: userID 決め打ち
+                router.push(`/hoge/${articleID}`);
+                setPostDisabled(false);
+            } else {
+                console.error('POST unknown state', data);
+            }
+        },
+        [content, subject, postDisabled, postArticle, router],
+    );
+
     const siteHeaderRight = (
         <>
             <SiteHeaderItem>
-                <Button type="submit">
-                    <span>投稿する</span>
-                </Button>
+                <form onSubmit={handlePost}>
+                    <Button type="submit">
+                        <span>投稿する</span>
+                    </Button>
+                </form>
             </SiteHeaderItem>
             <SiteHeaderItem>
                 <img className={styles.userIcon} src="/profile.png" />
@@ -49,11 +83,6 @@ const PostPage: NextPage = () => {
                     onEdit={setContent}
                 />
             </div>
-            <footer className={styles.footer}>
-                <Button className={styles.submitButton} type="submit">
-                    投稿する
-                </Button>
-            </footer>
         </>
     );
 };
