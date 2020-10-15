27e6fa33ee6ef047930d339bf5497401fb6a90a1
yusei-wy <yusei.kasa@gmail.com>
2020-10-15 11:22:49 +0900

    change: リファクタリング4 記事フォーマットを関数化

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -4,6 +4,8 @@ import { useMemo } from 'react';
  * 複数のクラス名を合成して1つの文字列に結合する
  * undefined が混じっていてもOK
  */
-export const useClassNames = (...names: ReadonlyArray<string | undefined>) => {
+export const useClassNames = (
+    ...names: ReadonlyArray<string | undefined>
+): string => {
     return useMemo(() => names.filter((name) => !!name).join(' '), [names]);
 };
