87d33f45768dd3f2e6a5a78711b810ebc101c181
yusei-wy <yusei.kasa@gmail.com>
2020-10-16 08:59:57 +0900

    change: リファクタリング5 article page でコンポーネントの分割

--- src/pages/[userID]/[articleID]/index.tsx
+++ src/pages/[userID]/[articleID]/index.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { useRouter } from 'next/router';
 import { NextPage } from 'next';
 import Error from 'next/error';
 
-import { useGetArticleQuery } from '@/generated/graphql';
+import { useGetArticleQuery, Users } from '@/generated/graphql';
 
 import styles from './index.module.css';
 import { Article } from '@/components/article';
@@ -10,6 +11,48 @@ import { SiteHeader } from '@/components/site-header';
 import { UserIcon } from '@/components/user-icon';
 import { formatDate } from '@/utils/date';
 import { useDIContext } from '@/context';
+
+type ArticleHeaderProps = {
+    subject: string;
+    user: Pick<Users, 'displayID' | 'displayName'>;
+    publishedAt: string;
+};
+
+const ArticleHeader: React.FC<ArticleHeaderProps> = ({
+    subject,
+    user,
+    publishedAt,
+}) => {
+    const services = useDIContext((services) => services);
+    const { datetime, isNew } = formatDate(
+        services.datetimeService.getNowByString(publishedAt),
+        services.datetimeService.getNow(),
+    );
+
+    return (
+        <>
+            <h1 className={styles.subject}>{subject}</h1>
+            <div className={styles.userContainer}>
+                <div>
+                    <UserIcon src="/profile.png" />
+                </div>
+                <div className={styles.userText}>
+                    <div className={styles.userID}>
+                        {user.displayName} @{user.displayID}
+                    </div>
+                    <span className={styles.publishedAt}>
+                        <span>{datetime}</span>
+                        {isNew ? (
+                            <span className={styles.newContent}>New</span>
+                        ) : (
+                            ''
+                        )}
+                    </span>
+                </div>
+            </div>
+        </>
+    );
+};
 
 const ArticlePage: NextPage = () => {
     const services = useDIContext((services) => services);
@@ -37,34 +80,13 @@ const ArticlePage: NextPage = () => {
     if (!publishedAt) {
         return <Error statusCode={404} />;
     }
-    const { datetime, isNew } = formatDate(
-        services.datetimeService.getNowByString(publishedAt),
-        services.datetimeService.getNow(),
-    );
+    const articleHeaderProps = { user, subject, publishedAt };
 
     return (
         <>
             <SiteHeader />
             <div className={styles.contentContainer}>
-                <h1 className={styles.subject}>{subject}</h1>
-                <div className={styles.userContainer}>
-                    <div>
-                        <UserIcon src="/profile.png" />
-                    </div>
-                    <div className={styles.userText}>
-                        <div className={styles.userID}>
-                            {user.displayName} @{user.displayID}
-                        </div>
-                        <span className={styles.publishedAt}>
-                            <span>{datetime}</span>
-                            {isNew ? (
-                                <span className={styles.newContent}>New</span>
-                            ) : (
-                                ''
-                            )}
-                        </span>
-                    </div>
-                </div>
+                <ArticleHeader {...articleHeaderProps} />
                 <div className={styles.content}>
                     <Article content={content} />
                 </div>
